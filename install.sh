e0cd6cbc55e51986b23a8e1300376880be1593a7
Girijesh Kaushik <girijeshk@gmail.com>
2020-07-11 14:14:44 +0530

    changed the install.sh for vim and tmux configuration installation.

--- install.sh
+++ install.sh
@@ -1,3 +1,35 @@
 #!/bin/bash
 
 # This script will install this environment for the current user.
+
+# move the current directorty to root location of my config
+cd `dirname $0`
+
+# save full path of root location
+ROOTDIR=`pwd`
+
+if [ "--run" !=  "$1" ]; then
+	DRY=1;
+	echo "This is a dry run. Use '$0 --run' for actual install. Work on to fix all the warnings before going for actual install.";
+fi
+
+warn() {
+	echo "Warning: $1";
+}
+
+createAliasIfSafe() {
+	if [ -e $2 ] || [ -L $2 ]; then
+		warn "$2 already exists. Please remove it first and re-run the script";
+	elif [ -z "$DRY" ]; then
+		echo "creating link of $1 as $2";
+		ln -sT $1 $2;
+	fi
+}
+
+# vim configuration
+createAliasIfSafe $ROOTDIR/vim ~/.vim
+createAliasIfSafe $ROOTDIR/vim/vimrc ~/.vimrc
+
+# tmux configuration
+createAliasIfSafe $ROOTDIR/tmux/tmux.conf ~/.tmux.conf
+
